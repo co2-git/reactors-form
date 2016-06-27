afaf8c26724796075a595b91a6c4fbb1c3e02349
co2-git <francoisrvespa@gmail.com>
2016-06-26 22:04:16 -0500

    Fix #8 Update package version to v0.0.1

--- app/components/TextInput/web.js
+++ app/components/TextInput/web.js
@@ -4,11 +4,6 @@ export default (props) => {
   const webProps = {...props};
   const onChange = webProps.onChange;
   delete webProps.onChange;
-  const onKeyUp = (event) => {
-    if (typeof onChange === 'function') {
-      onChange(event.target.value);
-    }
-  };
   return <input
     type="text"
     {...webProps}
