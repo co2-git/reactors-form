b9a29c612408081d1602cc07a9fc635efea6a889
co2-git <francoisrvespa@gmail.com>
2016-10-10 12:29:19 -0500

    dropdown in index

--- app/index.js
+++ app/index.js
@@ -1,2 +1,3 @@
 export {default as TextInput} from './components/TextInput';
 export {default as Button} from './components/Button';
+export {default as Dropdown} from './components/Dropdown';
--- dist/index.js
+++ dist/index.js
@@ -22,4 +22,13 @@ Object.defineProperty(exports, 'Button', {
   }
 });
 
+var _Dropdown = require('./components/Dropdown');
+
+Object.defineProperty(exports, 'Dropdown', {
+  enumerable: true,
+  get: function get() {
+    return _interopRequireDefault(_Dropdown).default;
+  }
+});
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
